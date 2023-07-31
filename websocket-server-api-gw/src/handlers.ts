e6a25621c762d87bf9de50addb4bf7b26028b9d2
rohanpatel01 <rohanppatel01@gmail.com>
2023-07-31 10:44:57 -0500

    LED Works

---
+++ websocket-server-api-gw/src/handlers.ts
@@ -0,0 +1,110 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DeleteItemCommand, DynamoDBClient, PutItemCommand, ScanCommand } from '@aws-sdk/client-dynamodb'
+import { ApiGatewayManagementApi, GoneException } from "@aws-sdk/client-apigatewaymanagementapi"
+import { TextEncoder } from "util";
+import { send } from "process";
+
+
+const responseOK = {
+  statusCode: 200,
+  body: "",
+};
+
+const dynamodbClient = new DynamoDBClient({});
+const apiGatewayManagementApi = new ApiGatewayManagementApi({
+  endpoint: process.env["WSSAPIGATEWAYENDPOINT"],
+})
+const clientsTable = process.env["CLIENTS_TABLE_NAME"] || "";
+const textEncoder = new TextEncoder();
+
+export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const connectionId = event.requestContext.connectionId as string;
+  const routeKey = event.requestContext.routeKey as string;
+  const body = event.body || "";
+
+
+  switch (routeKey) {
+    case "$connect":
+      return handleConnect(connectionId);
+    case "$disconnect":
+      return handleDisconnect(connectionId);
+    case "msg":
+      return handleMsg(connectionId, body);
+  }
+
+  return responseOK; // changed
+};
+
+
+const handleConnect = async (connectionId: string): Promise<APIGatewayProxyResult> => {
+  await dynamodbClient.send(
+    new PutItemCommand({
+      TableName: clientsTable,
+      Item: {
+        connectionId: {
+          S: connectionId,
+        },
+      },
+    }),
+  );
+
+  // sendMessage(connectionId, JSON.stringify({ action: "msg", type: "warning", body: "no recipient" }))
+  // console.log("send message inside connect")
+
+  return responseOK;
+}
+
+const handleDisconnect = async (connectionId: string): Promise<APIGatewayProxyResult> => {
+  await dynamodbClient.send(
+    new DeleteItemCommand({
+      TableName: clientsTable,
+      Key: {
+        connectionId: {
+          S: connectionId,
+        },
+      },
+    }),
+  );
+
+  return responseOK;
+
+};
+
+
+const handleMsg = async (thisConnectionId: string, body: string): Promise<APIGatewayProxyResult> => {
+  const output = await dynamodbClient.send(
+    new ScanCommand({
+      TableName: clientsTable,
+    }),
+  );
+
+  if (output.Count && output.Count > 0) {
+    for (const item of output.Items || []) {
+      if (item["connectionId"].S !== thisConnectionId) {
+        sendMessage(item["connectionId"].S as string, body);
+        console.log("sent message")
+      }
+    }
+
+  } else {
+    await sendMessage(thisConnectionId, JSON.stringify({ action: "msg", type: "warning", body: "no recipient" }));
+  }
+
+  return responseOK;
+};
+
+const sendMessage = async (connectionId: string, body: string) => {
+  try{
+    await apiGatewayManagementApi.postToConnection({
+      ConnectionId: connectionId,
+      Data: textEncoder.encode(body),
+    });
+  } catch (e) {
+    if (e instanceof GoneException) {
+      await handleDisconnect(connectionId);
+      return;
+    }
+
+    throw e;
+  }
+};
